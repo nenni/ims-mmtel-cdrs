268123601e7bb4e3ac6a093294300615b92daa90
nenni <nikolay.nenchev@gmail.com>
2015-02-03 16:37:54 +0000

    remove mmtelrecords, and user tagClassApplication organize mmtel.py into cdr_write and encode pdu functions

--- mmtel.py
+++ mmtel.py
@@ -1,7 +1,6 @@
 #!/usb/bin/python
 
 from pyasn1.codec.ber import encoder, decoder
-
 from modules import mmtel3gpp32298 as mmtel
 
 from time import sleep
@@ -9,43 +8,46 @@ from time import sleep
 
 '''
 Test to encode and decoder IMS MMTel CDR ber files
-Using just a subset of MMTelRecord ASN1 definition defined in 3GPP TS 32.298
 '''
 
 def encode_pdu():
         pdu = mmtel.MMTelRecord()
-        print "Strt of cdr log"
+        # print "Strt of cdr log"
         pdu.setComponentByPosition(0, mmtel.RecordType(83), '')
-        pdu.setComponentByPosition(2, mmtel.SIPMethod('INVITE'), '')
+        pdu.setComponentByPosition(2, mmtel.SIPMethod('REGISTER'), '')
         pdu.setComponentByPosition(3, mmtel.RoleOfNode(0), '')
         print pdu
-        print "End of cdr log"
+        # print "End of cdr log"
         w_data = encoder.encode(pdu)
         return w_data
 
 
 def cdr_write(file):
     print "Filename: " + file
-    with open(file, 'a+b') as f:
+    with open(file, 'w+b') as f:
         f.write(encode_pdu())
         f.close
 
 
 def cdr_read(file):
     with open(file, 'rb') as f:
-        r_data = f.read
+        r_data = f.read()
         f.close()
+        print r_data
         decoder.decode.defaultErrorState = decoder.stDumpRawValue
-        data = decoder.decode(r_data, asn1Spec=mmtel.MMTelRecord)
+        #data = mmtel.MMTelRecord()
+        data = decoder.decode(r_data, asn1Spec=mmtel.MMTelRecord())
         print data
 
 
 if __name__ == "__main__":
 
-    file = 'mcdrtagclassapp4.ber'
+    file = 'mcdr10.ber'
     cdr_write(file)
     # cdr_write(file)
-    # cdr_read(file)
+
+    sleep(2)
+    cdr_read(file)
 
 
 
--- modules/mmtel3gpp32298.py
+++ modules/mmtel3gpp32298.py
@@ -10,16 +10,16 @@ Using just a subset of MMTelRecord ASN1 definition defined in 3GPP TS 32.298
 
 
 class RecordType(univ.Integer):
-    tagSet = univ.Integer.tagSet.tagImplicitly(tag.Tag(tag.tagClassApplication, tag.tagFormatSimple, 0))
+    tagSet = univ.Integer.tagSet.tagImplicitly(tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0))
     namedValues = namedval.NamedValues(('mMTelRecord', 83))
 
 
 class SIPMethod(char.GraphicString):
-        tagSet = char.GraphicString.tagSet.tagImplicitly(tag.Tag(tag.tagClassApplication, tag.tagFormatSimple, 2))
+        tagSet = char.GraphicString.tagSet.tagImplicitly(tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 2))
 
 
 class RoleOfNode(univ.Enumerated):
-    tagSet = univ.Enumerated.tagSet.tagImplicitly(tag.Tag(tag.tagClassApplication, tag.tagFormatSimple, 3))
+    tagSet = univ.Enumerated.tagSet.tagImplicitly(tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 3))
     namedValues = namedval.NamedValues(
         ('originating', 0),
         ('terminating', 1)
